2af52a7e0ee88010ea73ead1193c809c3546aa89
StrangeExplodeWallnut <102153789+StrangeExplodeWallnut@users.noreply.github.com>
2022-04-02 11:38:01 +0800

    Add files via upload

---
+++ PrimeMaker/PrimeMakerFree.py
@@ -0,0 +1,21 @@
+def CreatePrime(n):
+    p=[]
+    for i in range(2,n+1):
+        flag=False
+        for j in range(2,i):
+            if i % j == 0:
+                flag=True
+        if flag==False:
+            p.append(i)
+    return p
+print('PrimeMakerFree')
+a=int(input('Before:'))
+fn=input('FileName:')
+if fn == '':
+    fn = 'Prime In '+str(a)
+dat=CreatePrime(a)
+f=open(fn+'.txt','w')
+f.write(str(dat)[1:-1])
+f.close()
+print(str(dat)[1:-1])
+e=input()
